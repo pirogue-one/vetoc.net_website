04648d7551e475cbcf67376aab229ada1cad245b
Alyona <alyona.evg@yandex.ru>
2023-09-09 13:18:06 +0300

    finished gallery carousel

--- gallery.js
+++ gallery.js
@@ -4,15 +4,44 @@ const galleryImages = document.querySelectorAll('.portfolio__item');
 const popup = document.querySelector('.popup-image');
 const popupImage = popup.querySelector('img');
 const closeBtn = document.querySelector('#close');
+const arrowPrev = document.querySelector('.arrow-prev');
+const arrowNext = document.querySelector('.arrow-next');
 
-galleryImages.forEach((item)=> {
+let currentIndex = 0;
+
+galleryImages.forEach((item, index)=> {
     item.addEventListener('click', () => {
         let source = item.getAttribute('src');
         popupImage.src = source;
         popup.classList.toggle('popup-hidden');
+        currentIndex = index;
     });
 })
 
 closeBtn.addEventListener('click', () => {
     popup.classList.toggle('popup-hidden');
+})
+
+
+
+arrowPrev.addEventListener('click', () => {
+    if (currentIndex == 0) {
+        currentIndex = galleryImages.length - 1;
+      } else {
+        currentIndex--;
+      }
+
+      popupImage.src = galleryImages[currentIndex].getAttribute('src');
+    
+})
+
+arrowNext.addEventListener('click', () => {
+    if (currentIndex == galleryImages.length - 1) {
+        currentIndex = 0;
+      } else {
+        currentIndex++;
+      }
+
+      popupImage.src = galleryImages[currentIndex].getAttribute('src');
+    
 })
